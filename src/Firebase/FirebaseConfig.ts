a9b24f8de8dc748f4f4abfd6bc12d54e9f9bfd82
Minminjamin <0429el@gmail.com>
2023-04-01 21:45:18 +0900

    join add

--- src/Firebase/FirebaseConfig.ts
+++ src/Firebase/FirebaseConfig.ts
@@ -2,26 +2,26 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_API_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_API_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_API_APP_ID,
-  measurementId: process.env.REACT_APP_API_MEASUREMENT_ID
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_API_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_API_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_API_APP_ID,
+    measurementId: process.env.REACT_APP_API_MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+const db = getFirestore(app); 
 
-export default initializeApp(firebaseConfig);
+export { app, analytics, auth, db };
--- src/component/Login/Join.tsx
+++ src/component/Login/Join.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Join = () => {
+  const [name, setName] = useState<string>("");
+  const [id, setId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const onHandleJoin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      // await Firebase.auth()
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <span>This is Join Page</span>
+      <form onSubmit={onHandleJoin}>
+        <div>
+          <label>이름</label>
+          <input
+            placeholder="이름을 입력해주세요."
+            type="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>아이디(ID)</label>
+          <input
+            placeholder="아이디를 입력해주세요."
+            type="id"
+            name="id"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>비밀번호(Password)</label>
+          <input
+            placeholder="비밀번호를 입력해주세요."
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>비밀번호 확인</label>
+          <input
+            placeholder="위에서 입력한 비밀번호를 다시 한 번 입력해주세요."
+            type="confirmPassword"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">회원가입 하기</button>
+      </form>
     </div>
   );
 };
