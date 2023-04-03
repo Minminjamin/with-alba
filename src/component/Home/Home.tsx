9b1ea1169031c3d8007f098e9b1f4eaab51db8ad
Minminjamin <0429el@gmail.com>
2023-04-03 19:22:48 +0900

    feat text add

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -3,7 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <span>This is Home</span>
+      <div>
+        <h5>전국에 알바 찾는 사람 모여요!</h5>
+        <span>
+          10대 후반부터 24세까지의 '청소년'층이 할 수 있는 아르바이트를
+          찾아보아요!
+        </span>
+        <span>여러분의 갓생과 좋은 일자리, 알바 구직 성공을 기원합니다.</span>
+      </div>
+
+      <div>
+        <button>+</button>
+      </div>
     </div>
   );
 };
