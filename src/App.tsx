74ec7b8ccc0e2966a5af86d03b91f21d929b06ed
Minminjamin <0429el@gmail.com>
2023-04-10 11:06:43 +0900

    app.tsx 주석 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
@@ -7,22 +7,7 @@ import Login from "./pages/Login/Login";
 import MyPage from "./pages/MyPage/MyPage";
 import Editor from "./pages/Editor/Editor";
 
-// import { firestore } from "./Firebase/FirebaseConfig";
-// import { collection, getDocs } from "firebase/firestore";
-
 function App() {
-  // const [test, setTest] = useState([]);
-  // const usersCollectionRef = collection(firestore, "posting");
-  // useEffect(() => {
-  //   // 비동기로 데이터 받을준비
-  //   const getUsers = async () => {
-  //     // getDocs로 컬렉션안에 데이터 가져오기
-  //     const data = await getDocs(usersCollectionRef);
-  //     console.log(data);
-  //   };
-
-  //   getUsers();
-  // }, []);
   return (
     <BrowserRouter>
       <div className="mx-24 my-5 relative">
