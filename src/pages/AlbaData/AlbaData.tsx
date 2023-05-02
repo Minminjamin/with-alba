67851e06dd3c41f1b1b4daa7027847b7852ae4e4
Minminjamin <0429el@gmail.com>
2023-05-02 19:04:22 +0900

    FEAD : During work

--- src/pages/AlbaData/AlbaData.tsx
+++ src/pages/AlbaData/AlbaData.tsx
@@ -1,12 +1,33 @@
-import React, { useEffect } from "react";
+import {
+  collection,
+  collectionGroup,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "@firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { firestore } from "../../api/Firebase/FirebaseConfig";
 
 const AlbaData = () => {
   const { id } = useParams();
+  const [albaData, setAlbaData] = useState<any>(null);
 
   useEffect(() => {
-    console.log(id);
-  }, []);
+    const getPostingData = async () => {
+      // const postingData = collection(firestore, "posting");
+      // const postingDoc = doc(postingData, id);
+      // const getPostingData = await getDoc(postingDoc);
+      // if (getPostingData.exists()) {
+      //   setAlbaData(getPostingData.data());
+      //   console.log("this is ", albaData);
+      // }
+    };
+
+    getPostingData();
+  }, [id]);
 
   return (
     <div>
