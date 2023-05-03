16762a78c9ff26acfc58f3f005fa3f280db6a88c
Minminjamin <0429el@gmail.com>
2023-05-03 21:55:55 +0900

    FEAT : 데이터 받아오기

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import { collection, getDocs } from "@firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { firestore } from "../../api/Firebase/FirebaseConfig";
 
 const MyPage = () => {
+  const { userId } = useParams();
+  const [myData, setMyData] = useState<any | string>();
+
+  useEffect(() => {
+    const getMyPostings = async () => {
+      const myPostingSnapShot = await getDocs(
+        collection(firestore, `db/${userId}/posting`)
+      );
+
+      const myPostingData = myPostingSnapShot.docs.map((doc) => doc.data());
+      await setMyData(myPostingData);
+    };
+
+    getMyPostings();
+  }, []);
   return (
     <div>
       <h6>내가 쓴 공고 보기</h6>
-      {/* <span>This is My MyPage</span> */}
     </div>
   );
 };
