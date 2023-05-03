08c27e67fa8176821b0896110774d09863dd249e
Minminjamin <0429el@gmail.com>
2023-05-03 19:39:19 +0900

    FEAT : 내가 쓴 공고 보기 추가

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const MyPage = () => {
   return (
     <div>
-      <span>This is My MyPage</span>
+      <h6>내가 쓴 공고 보기</h6>
+      {/* <span>This is My MyPage</span> */}
     </div>
   );
 };
