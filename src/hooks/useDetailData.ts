17b6a5590d2196efe2b0a412bc77e812c85937e6
Minminjamin <0429el@gmail.com>
2023-05-05 20:45:01 +0900

    FEAT  :Add useDetailData hook

---
+++ src/hooks/useDetailData.ts
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { firestore } from "../api/Firebase/FirebaseConfig";
+import { doc, getDoc } from "@firebase/firestore";
+
+const useDetailData = (user: string, title: string) => {
+  const [data, setData] = useState<string | any>();
+
+  useEffect(() => {
+    const getDatas = async () => {
+      const docRef = doc(firestore, `db/${user}/posting/${title}`);
+
+      getDoc(docRef).then((docSnap) => {
+        if (docSnap.exists()) {
+          setData(docSnap.data());
+        }
+      });
+    };
+
+    getDatas();
+  }, [user, title]);
+
+  return [data];
+};
+
+export default useDetailData;
