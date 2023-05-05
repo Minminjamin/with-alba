187ebde695eb09c75c7f140be5c32215cbf6a39e
Minminjamin <0429el@gmail.com>
2023-05-05 11:15:10 +0900

    REFACT : useInput text state에 타입 추가

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const useInput = () => {
-  const [text, setText] = useState();
+  const [text, setText] = useState<any | null | string | null | number>();
   const onChange = (event: any) => {
     const {
       target: { value: text },
