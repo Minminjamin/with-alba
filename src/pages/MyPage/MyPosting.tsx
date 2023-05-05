34de63d79b51de673b1c098bcb716f449a7ed4fc
Minminjamin <0429el@gmail.com>
2023-05-05 20:58:46 +0900

    REFA : 사용하지 않은 모듈 제거

--- src/pages/MyPage/MyPosting.tsx
+++ src/pages/MyPage/MyPosting.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { firestore } from "../../api/Firebase/FirebaseConfig";
 import { deleteDoc, doc, getDoc } from "@firebase/firestore";
-import { async } from "@firebase/util";
 
 const MyPosting = () => {
   const { userId, title } = useParams<any>();
