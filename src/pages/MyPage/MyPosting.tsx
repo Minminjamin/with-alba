4a1e8f5f54d1c902e68ea3e7e7f4a0c455eb827e
Minminjamin <0429el@gmail.com>
2023-05-06 21:37:45 +0900

    REFA : Add NoData component

--- src/pages/MyPage/MyPosting.tsx
+++ src/pages/MyPage/MyPosting.tsx
@@ -4,6 +4,7 @@ import { firestore } from "../../api/Firebase/FirebaseConfig";
 import { deleteDoc, doc } from "@firebase/firestore";
 import useDetailData from "../../hooks/useDetailData";
 import AlbaData from "../AlbaData";
+import NoData from "../NoData";
 
 const MyPosting = () => {
   const { userId, title } = useParams<any>();
@@ -34,15 +35,7 @@ const MyPosting = () => {
           </div>
         </div>
       ) : (
-        <div className="flex justify-center flex-col text-center space-y-5 mt-36">
-          <span className="w-full">데이터가 없습니다.</span>
-          <span className="w-full font-bold text-red-600">
-            잠시 기다려주세요.
-          </span>
-          <span className="w-full">
-            그래도 데이터가 나타나지 않으면 새로고침을 해주세요.
-          </span>
-        </div>
+        <NoData />
       )}
     </div>
   );
