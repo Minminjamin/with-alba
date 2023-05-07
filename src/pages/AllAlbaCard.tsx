caa97651d3e23177a18058f14d07fe16627c34a7
Minminjamin <0429el@gmail.com>
2023-05-08 08:30:21 +0900

    STYLE : 주석 제거

--- src/pages/AllAlbaCard.tsx
+++ src/pages/AllAlbaCard.tsx
@@ -15,7 +15,6 @@ const AllAlbaCard = () => {
       );
       const data = querySnapshot.docs.map((doc) => doc.data());
       setPostingData(data);
-      // await console.log(data);
     };
 
     getPostings();
@@ -28,20 +27,6 @@ const AllAlbaCard = () => {
   return (
     <div className="flex grid-rows-3 justify-around">
       <PostingCard postingData={postingData} clickEvent={HandleClickPosting} />
-      {/* {postingData.map((posting: any) => (
-        <div
-          key={posting.title}
-          onClick={(e) => HandleClickPosting(e, posting.title, posting.userId)}
-          className="rounded-lg border-2 w-1/4 flex flex-col items-start h-52 content-between justify-around shadow-lg cursor-pointer"
-        >
-          <h3 className="font-bold text-xl text-center w-full justify-items-center">
-            {posting.title}
-          </h3>
-          <span className="ml-4">{posting.deadline}</span>
-          <span className="ml-4">나이 : {posting.age}</span>
-          <span className="ml-4">{posting.address}</span>
-        </div>
-      ))} */}
     </div>
   );
 };
