a92acf3923b3da66921697ab12663b15ed4906f3
Minminjamin <0429el@gmail.com>
2023-05-11 13:13:51 +0900

    FEAT : 자동 로그인 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./routes/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
@@ -8,8 +8,30 @@ import MyPage from "./routes/MyPage";
 import Editor from "./routes/Editor";
 import ShowAlbaData from "./routes/ShowAlbaData";
 import MyPosting from "./routes/MyPosting";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./api/Firebase/FirebaseConfig";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "./store/modules/isLogin/isLoginAction";
 
 function App() {
+  const dispatch = useDispatch();
+  useSelector((state: any) => state.isLogin.isLogin);
+
+  useEffect(() => {
+    const autoLogin = async () => {
+      const user = JSON.parse(localStorage.getItem("user"));
+
+      if (user) {
+        const id: string = user.id;
+        const password: string = user.password;
+
+        await signInWithEmailAndPassword(auth, id, password);
+        dispatch(login());
+      }
+    };
+    autoLogin();
+  }, []);
+
   return (
     <BrowserRouter>
       <div className=" lg:mx-24 lg:mt-10">
